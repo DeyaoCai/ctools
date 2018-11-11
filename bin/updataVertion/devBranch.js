4903a977defd16f131aaec36d3aab610c84adbe9
DeyaoCai <32273577+DeyaoCai@users.noreply.github.com>
2018-11-11 21:55:14 +0800

    auto commit

---
+++ bin/updataVertion/devBranch.js
@@ -0,0 +1,14 @@
+const exec = require("../../src/exec.js");
+const updatePackageJson = require("./updatePackageJson.js");
+const fns = {
+  // 发布
+  publish: {
+    exec: "npm publish",
+    next: ()=> fns.xxx,
+  },
+}
+// 入口；
+module.exports =  function () {
+  updatePackageJson();
+  exec(fns.publish)
+};
