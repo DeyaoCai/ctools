0eb2be4cb2ef945556a4a0eabc5e6df0d9d6f208
DeyaoCai <32273577+DeyaoCai@users.noreply.github.com>
2018-11-12 23:19:37 +0800

    auto commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import copy from "../copy.js"
+import Drag from "../Drag.js"
+import getClass from "../getClass.js"
+import getStyle from "../getStyle.js"
+import type from "../type.js"
+
+export default {
+	copy,
+	Drag,
+	getClass,
+	getStyle,
+	type
+}
