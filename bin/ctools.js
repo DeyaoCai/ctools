775836b28c0cdd3a2842e14f2cf248319c5cceb0
DeyaoCai <32273577+DeyaoCai@users.noreply.github.com>
2018-11-12 22:59:33 +0800

    auto commit

--- bin/ctools.js
+++ bin/ctools.js
@@ -1,4 +1,17 @@
 #! node
+/*
+    dev参数
+    "push" 发布版本；
+    "push --push" // 提交代码并发布版本
+    "push --push --not-publish" //提交代码不发布版本
+
+    分支参数
+    "push" 提交代码
+    "push --todev",  // 提交代码并合并到dev
+    "push --todev --publish", // 提交代码并合并到dev 并发布版本
+
+      为什么一个分支参数做加法一个做减法 因为之前的设计就是 dev发版本 ISSUES 提交代码啊；
+*/
 const fs = require("fs");
 const log = require("../src/log.js");
 const exec = require("../src/exec.js");
