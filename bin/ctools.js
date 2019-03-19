908b5db267055643e8ba82d66f27a6c1e7fc2492
DeyaoCai <32273577+DeyaoCai@users.noreply.github.com>
2019-03-20 01:52:29 +0800

    no message

--- bin/ctools.js
+++ bin/ctools.js
@@ -4,27 +4,58 @@ const log = require("../src/log.js");
 const exec = require("../src/exec.js");
 const read = require("../src/read.js");
 const readWx = require("../src/readWx.js");
+const path = require("path");
 
+// const aaa = require()
 const cwd = process.cwd();
 const arv = process.argv;
+console.log(
+  path.join(__dirname, "")
+)
 
-if(arv.includes("read")) {
-  let conf;
-  const pathes = fs.readdirSync("./");
-  if (pathes.includes("ctools.config.js")){
-    try{conf = require(cwd + "/ctools.config.js");}
-    catch (e) {
-      log.t("get 'ctools.config.js' fail!");
-      throw(e);
-    }
-    conf.forEach(item => {
-      if(item.readType === "single file") readWx.writeExportFile(item) ;
-      else read.writeExportFile(item) ;
-    });
-  } else {
-    throw "we need a 'read.config.js' file";
-  }
+let ctoolsUsrConf;
+try {
+  const ctoolsConfPath = `${cwd}/ctools.config.js`;
+  ctoolsUsrConf = require(ctoolsConfPath);
+} catch (e) {
+  console.log("读取配置失败， 将使用默认配置！");
+  ctoolsUsrConf = {};
 }
-if(arv.includes("proxy")) {
+const ctoolsConf = Object.assign({
+  push: {},
+  read: {},
+  watch: {},
+}, ctoolsUsrConf);
+
+if (arv.includes("read")) {
+  const conf = ctoolsConf.read;
+  Object.keys(conf).forEach(item => {
+    conf && conf[item].forEach(item => {
+      if (item.readType === "single file") readWx.writeExportFile(item);
+      else read.writeExportFile(item);
+    });
+  });
+}
+const readConfReg = /^read--/;
+const readConf = arv.filter(item => readConfReg.test(item));
+
+if (readConf.length) {
+  const conf = ctoolsConf.read;
+  readConf.forEach(item => {
+    const arr = item.replace(readConfReg, "").split(/-/);
+    arr.forEach(item => {
+      conf && conf[item] && conf[item].forEach(item => {
+        if (item.readType === "single file") readWx.writeExportFile(item);
+        else read.writeExportFile(item);
+      });
+    })
+  });
+}
+
+if (arv.includes("proxy")) {
   exec({exec: "node ./src/node/app.js"});
+}
+if (arv.includes("watch")) {
+  console.log("watch");
+  // exec({exec: "node ./src/node/app.js"});
 }
