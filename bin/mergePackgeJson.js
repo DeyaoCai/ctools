e9b1cb9e690795dd0122b3a13f4d3f52bf47ba3e
DeyaoCai <32273577+DeyaoCai@users.noreply.github.com>
2019-03-20 21:13:28 +0800

    no message

--- bin/mergePackgeJson.js
+++ bin/mergePackgeJson.js
@@ -7,11 +7,19 @@ const readTemMainTemp = require("../src/readTemMainTemp.js");
 
 const cwd = process.cwd();
 const arv = process.argv;
-const repertoryDirName = "tem-biz";
+let repertoryDirName;
+try {
+  repertoryDirName = require(`${cwd}/ctools.conf/webpack.conf.js`).repertoryPath;
+} catch (e) {
+  repertoryDirName = "tem-biz";
+}
 let baseMap;
 try {
   baseMap = require(`${cwd}/ctools.conf/webpack.conf.js`).alias;
-} catch (e) { baseMap = {}};
+} catch (e) {
+  baseMap = {}
+}
+;
 
 function setWebPackConfAlias(name, conf, fullPath) {
   const map = baseMap[name] || {};
@@ -28,44 +36,44 @@ function getPackage(list = []) {
   confList.forEach((conf, index) => {
     const name = conf.name;
     name && setWebPackConfAlias(name, webpackConfAlias, list[index]);
-    const dependencies =  conf.dependencies;
-    const devDependencies =  conf.devDependencies;
-    const repository =  conf.repository;
+    const dependencies = conf.dependencies;
+    const devDependencies = conf.devDependencies;
+    const repository = conf.repository;
     dependencies && Object.keys(dependencies).forEach(key =>
-      baseConf.dependencies[key] = dependencies[key]
+      (baseConf.dependencies[key] = dependencies[key])
     );
     devDependencies && Object.keys(devDependencies).forEach(key =>
-      baseConf.dependencies[key] = devDependencies[key]
+      (baseConf.dependencies[key] = devDependencies[key])
     );
     repository && Object.keys(repository).forEach(key =>
-      baseConf.repository[key] = repository[key]
+      (baseConf.repository[key] = repository[key])
     );
   });
   fs.writeFileSync(`${cwd}/package.json`, JSON.stringify(baseConf));
   console.log(`write 'package.json' success!`);
 
   fs.writeFileSync(`${cwd}/ctools.conf/webpack.conf.json`, JSON.stringify({
-    resolve: {extensions: ['.js', '.vue', '.json'],alias: webpackConfAlias}
+    resolve: {extensions: ['.js', '.vue', '.json'], alias: webpackConfAlias}
   }));
   console.log(`write 'webpack.conf.json' success!`);
 
   const excludeList = [`wxm-app`];
   readTemMainTemp.writeExportFile({
-    names: confList.map(item => item. name).filter(item => !excludeList.includes(item)),
+    names: confList.map(item => item.name).filter(item => !excludeList.includes(item)),
     outPutPath: `/src/main.js`,
   });
 }
 
-if(arv.includes("install")) {
+if (arv.includes("install")) {
   cProcess.execSync("npm install");
   console.log(`install package success!`);
 }
-if(arv.includes("updatePackageJson")) {
+if (arv.includes("updatePackageJson")) {
   getPackage();
   console.log(`update package success!`);
 }
 
-if(arv.includes("getCodes")) {
+if (arv.includes("getCodes")) {
   const packagePatth = [];
   const branchReg = /^--branch-/;
   const branchArv = arv.find(item => branchReg.test(item));
@@ -80,28 +88,32 @@ if(arv.includes("getCodes")) {
     const branch = userBranch || (branchArv && branchArv.replace(branchReg, ""));
     const branchRegExp = branch ? new RegExp(branch) : null;
 
-    const dirName = repertory.replace(/(.+)\/([^\/]+)\.git$/,`$2`);
+    const dirName = repertory.replace(/(.+)\/([^\/]+)\.git$/, `$2`);
     const outPutPath = `${cwd}/${repertoryDirName}/${dirName}`;
     packagePatth.push(outPutPath);
     try {
       cProcess.execSync(`git clone ${repertory}`);
       console.log(`clone '${repertory}' success!`);
-    } catch(e){}
+    } catch (e) {
+    }
 
     if (branchRegExp) {
       try {
         process.chdir(outPutPath);
         const result = cProcess.execSync(`git branch -r`, {encoding: 'utf8'});
         if (!branchRegExp.test(result)) {
-          try{
-            cProcess.execSync(`git checkout master`);
-          } catch(e) {}
           try {
-          cProcess.execSync(`git branch ${branch}`);
-          } catch(e) {}
+            cProcess.execSync(`git checkout master`);
+          } catch (e) {
+          }
+          try {
+            cProcess.execSync(`git branch ${branch}`);
+          } catch (e) {
+          }
         }
         cProcess.execSync(`git checkout ${branch}`);
-      } catch(e) {}
+      } catch (e) {
+      }
       process.chdir(`${cwd}/${repertoryDirName}`);
     }
 
