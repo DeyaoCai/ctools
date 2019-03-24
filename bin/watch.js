9021c5212645ca9fa3cd0caeae5bed3d23163e93
DeyaoCai <32273577+DeyaoCai@users.noreply.github.com>
2019-03-25 00:33:46 +0800

    no message

--- bin/watch.js
+++ bin/watch.js
@@ -1,25 +1,29 @@
 const fs = require("fs");
-const cprocess = require("child_process");
-
-function writeFiles() {
-  cprocess.exec("node ./bin/ctools.js read");
-  shouldExec = true;
-  console.log("succ");
+const path = require("path");
+const cProcess = require("child_process");
+const cwd = process.cwd();
+const conf = require(path.join(cwd, "./ctools.config"));
+const cb = makeExecLater(function () {
+  cProcess.execSync(`node ${path.join(__dirname, `./ctools.js`)} read"`);
+  console.log(`exec success, ${Date.now()}`);
+});
+const arr = flatArr(
+  flatArr(conf.read).map(item => item.inputPath)
+);
+function flatArr (arr){
+  return [].concat.apply([], Object.values(arr))
 }
 
-let shouldExec;
-writeFiles();
-
-function timeout() {
-  shouldExec && setTimeout(() => {
-    writeFiles();
-  }, 300);
-  shouldExec = false;
-}
-
-fs.unwatchFile("./oriSrc");
-fs.watch("./oriSrc", {}, function (a, b) {
-  lastTime = new Date().getTime();
-  timeout();
+arr.forEach(item => {
+  fs.watch(path.join(cwd, item), cb);
 });
 
+function makeExecLater(fn) {
+  let time = 0;
+  return function () {
+    const thisTime = time = `${Date.now()} ${Math.random()}`;
+    setTimeout(() => {
+      if (thisTime === time) fn.apply(null, arguments);
+    }, 340)
+  }
+}
