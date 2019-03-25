7a9b18e0d3c0af264d571abe406061ef58936245
DeyaoCai <32273577+DeyaoCai@users.noreply.github.com>
2019-03-26 05:20:53 +0800

    no message

--- bin/watch.js
+++ bin/watch.js
@@ -3,19 +3,30 @@ const path = require("path");
 const cProcess = require("child_process");
 const cwd = process.cwd();
 const conf = require(path.join(cwd, "./ctools.config"));
-const cb = makeExecLater(function () {
+const readCb = makeExecLater(function () {
   cProcess.execSync(`node ${path.join(__dirname, `./ctools.js`)} read"`);
+  console.log(`exec success, ${Date.now()}`);
+});
+const hotCb = makeExecLater(function () {
+  const hotPath = path.join(cwd, conf.hot);
+  console.log(`now start reload ${hotPath}!`);
+  cProcess.execSync(`npm run build"`);
   console.log(`exec success, ${Date.now()}`);
 });
 const arr = flatArr(
   flatArr(conf.read).map(item => item.inputPath)
 );
+if (conf.hot) {
+  const hotPath = path.join(cwd, conf.hot);
+  fs.watch(hotPath, hotCb);
+  console.log(`now watching: ${hotPath}`);
+}
 function flatArr (arr){
   return [].concat.apply([], Object.values(arr))
 }
 
 arr.forEach(item => {
-  fs.watch(path.join(cwd, item), cb);
+  fs.watch(path.join(cwd, item), readCb);
 });
 
 function makeExecLater(fn) {
